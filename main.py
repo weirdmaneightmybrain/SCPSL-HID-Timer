473185e6acf17c200e53f557bbca32ec5d4ae513
weirdmaneightmybrain <107234539+weirdmaneightmybrain@users.noreply.github.com>
2024-06-04 23:15:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+hid = open("HID_Sign.txt", "x")
+count = 0
+sec = 0
+min = 0
+mtfcount = 0
+elg = False
+length = 1200
+
+while count <= length:
+    if sec % 60 == 0 and count != 0:
+        sec -= 60
+        min += 1
+    if count % 280 == 0 and count != 0:
+        elg = True
+    if elg == True and mtfcount <= 70:
+        hid.write( f"{min:02d}:" + f"{sec:02d}" + " Spawn Eligible" + "\n")
+        hid.write( f"{min:02d}:" + f"{sec:02d}" + " Spawn Eligible" + "\n")
+        mtfcount += 1
+        count += 1
+        sec += 1
+    else:
+        elg = False
+        mtfcount = 0
+        hid.write( f"{min:02d}:" + f"{sec:02d}" + "\n")
+        hid.write( f"{min:02d}:" + f"{sec:02d}" + "\n")
+        count += 1
+        sec += 1
